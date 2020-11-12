86640c97d5e0c62df2c2d45d6b7a1299701a3a96
EHaze1999 <58138330+EHaze1999@users.noreply.github.com>
2020-11-12 13:32:00 +0100

    User + UserData classes
    Classes voor gebruikers data.

---
+++ project-c/Models/Users/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_c_plantjes
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public char Password { get; set; }
+        public enum Role { customer, admin }
+        public int Status { get; set; }
+        public DateTime Created { get; set; }
+
+        public User(int userid, string email, char password, int status, DateTime created)
+        {
+            UserID = userid;
+            Email = email;
+            Password = password;
+            Status = status;
+            Created = created;
+        }
+
+}
+}
---
+++ project-c/Models/Users/UserData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_c_plantjes.Models.Users
+{
+    public class UserData
+    {
+    }
+}
