63b8cd4c8f9aaf87f694cf182b7372b895c2f7ba
henkrehorst <henkrehorst@outlook.com>
2020-11-12 13:27:59 +0100

    Added postgres connection

---
+++ project-c/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace project_c
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        
+        //add models to database with: public DbSet<modelName> name {get; set;}
+    }
+}
