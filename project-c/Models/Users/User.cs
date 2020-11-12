5a708fb9d5a19f784abf2e8edafdd050652a560b
henkrehorst <henkrehorst@outlook.com>
2020-11-12 13:46:26 +0100

    Add database validation

--- project-c/Models/Users/User.cs
+++ project-c/Models/Users/User.cs
@@ -1,17 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace project_c.Models.Users
 {
     public class User
     {
         public int UserId { get; set; }
+        [DataType(DataType.EmailAddress), StringLength(255), Required]
         public string Email { get; set; }
-        public char Password { get; set; }
+        
+        [DataType(DataType.Password), Required, StringLength(64)]
+        public string Password { get; set; }
         public enum Role { Customer, Admin }
         public int Status { get; set; }
         public DateTime Created { get; set; }
 
-        public User(int userid, string email, char password, int status, DateTime created)
+        public User(int userid, string email, string password, int status, DateTime created)
         {
             UserId = userid;
             Email = email;
