295111e9e648155d7b21be0d6d95e8f7b1f995ca
henkrehorst <henkrehorst@outlook.com>
2020-11-12 13:34:33 +0100

    Fix namespace user models

--- project-c/Models/Users/User.cs
+++ project-c/Models/Users/User.cs
@@ -1,23 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace project_c_plantjes
+namespace project_c.Models.Users
 {
     public class User
     {
-        public int UserID { get; set; }
+        public int UserId { get; set; }
         public string Email { get; set; }
         public char Password { get; set; }
-        public enum Role { customer, admin }
+        public enum Role { Customer, Admin }
         public int Status { get; set; }
         public DateTime Created { get; set; }
 
         public User(int userid, string email, char password, int status, DateTime created)
         {
-            UserID = userid;
+            UserId = userid;
             Email = email;
             Password = password;
             Status = status;
--- project-c/Models/Users/UserData.cs
+++ project-c/Models/Users/UserData.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace project_c_plantjes.Models.Users
+﻿namespace project_c.Models.Users
 {
     public class UserData
     {
