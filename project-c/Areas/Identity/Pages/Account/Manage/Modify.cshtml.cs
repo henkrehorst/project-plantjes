6cfd0b31e27798c0e1b3a8cad484767897c31e71
EHaze1999 <58138330+EHaze1999@users.noreply.github.com>
2020-11-14 12:28:18 +0100

    Email op de Modify pagina
    Alvast het email adres op de modify pagina - Gebruikersnaam komt nog.

--- project-c/Areas/Identity/Pages/Account/Manage/Modify.cshtml.cs
+++ project-c/Areas/Identity/Pages/Account/Manage/Modify.cshtml.cs
@@ -2,53 +2,71 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using project_c.Models.Users;
 
 namespace project_c.Areas.Identity.Pages.Account.Manage
 {
+
+    // IN PLAATS VAN Console.WriteLine(), GEBRUIK System.Diagnostics.Debug.WriteLine(). CTRL+ALT+O
     public class ModifyModel : PageModel
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
         private readonly ILogger<ChangePasswordModel> _logger;
+        private readonly IEmailSender _emailSender;
 
         public ModifyModel(
             UserManager<User> userManager,
             SignInManager<User> signInManager,
-            ILogger<ChangePasswordModel> logger)
+            ILogger<ChangePasswordModel> logger,
+            IEmailSender emailSender)
+
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
+            _emailSender = emailSender;
         }
+        public string Username { get; set; }
 
-        [BindProperty] public InputModel Input { get; set; }
+        public string Email { get; set; }
 
-        [TempData] public string StatusMessage { get; set; }
+        public bool IsEmailConfirmed { get; set; }
 
-        public class InputModel
+        [TempData] 
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public EmailInputModel EmailInput { get; set; }
+
+        public class EmailInputModel
         {
             [Required]
-            [DataType(DataType.Password)]
-            [Display(Name = "Huidig wachtwoord")]
-            public string OldPassword { get; set; }
+            [EmailAddress]
+            [Display(Name = "New email")]
+            public string NewEmail { get; set; }
+        }
 
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
-                MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "Nieuw wachtwoord")]
-            public string NewPassword { get; set; }
+        private async Task LoadAsync(User user)
+        {
+            var email = await _userManager.GetEmailAsync(user);
+            Email = email;
 
-            [DataType(DataType.Password)]
-            [Display(Name = "Bevestig nieuw wachtwoord")]
-            [Compare("NewPassword", ErrorMessage = "De nieuwe wachtwoord velden zijn niet gelijk. Probeer het opnieuw.")]
-            public string ConfirmPassword { get; set; }
+            EmailInput = new EmailInputModel
+            {
+                NewEmail = email,
+            };
+
+            IsEmailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
         }
 
         public async Task<IActionResult> OnGetAsync()
@@ -65,38 +83,76 @@ namespace project_c.Areas.Identity.Pages.Account.Manage
                 return RedirectToPage("./SetPassword");
             }
 
+            await LoadAsync(user);
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostChangeEmailAsync()
         {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
-            var changePasswordResult =
-                await _userManager.ChangePasswordAsync(user, Input.OldPassword, Input.NewPassword);
-            if (!changePasswordResult.Succeeded)
+            if (!ModelState.IsValid)
             {
-                foreach (var error in changePasswordResult.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
-
+                await LoadAsync(user);
                 return Page();
             }
 
-            await _signInManager.RefreshSignInAsync(user);
-            _logger.LogInformation("User changed their password successfully.");
-            StatusMessage = "Uw wachtwoord is veranderd.";
+            var email = await _userManager.GetEmailAsync(user);
+            if (EmailInput.NewEmail != email)
+            {
+                var userId = await _userManager.GetUserIdAsync(user);
+                var code = await _userManager.GenerateChangeEmailTokenAsync(user, EmailInput.NewEmail);
+                var callbackUrl = Url.Page(
+                    "/Account/ConfirmEmailChange",
+                    pageHandler: null,
+                    values: new { userId = userId, email = EmailInput.NewEmail, code = code },
+                    protocol: Request.Scheme);
+                await _emailSender.SendEmailAsync(
+                    EmailInput.NewEmail,
+                    "Confirm your email",
+                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
+                StatusMessage = "Confirmation link to change email sent. Please check your email.";
+                return RedirectToPage();
+            }
+
+            StatusMessage = "Your email is unchanged.";
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostSendVerificationEmailAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+                return Page();
+            }
+
+            var userId = await _userManager.GetUserIdAsync(user);
+            var email = await _userManager.GetEmailAsync(user);
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            var callbackUrl = Url.Page(
+                "/Account/ConfirmEmail",
+                pageHandler: null,
+                values: new { area = "Identity", userId = userId, code = code },
+                protocol: Request.Scheme);
+            await _emailSender.SendEmailAsync(
+                email,
+                "Confirm your email",
+                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+            StatusMessage = "Verification email sent. Please check your email.";
             return RedirectToPage();
         }
     }
