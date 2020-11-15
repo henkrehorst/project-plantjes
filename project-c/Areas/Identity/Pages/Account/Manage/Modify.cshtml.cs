2043d3898edc02475844ff77497699b4651f7b0b
EHaze1999 <58138330+EHaze1999@users.noreply.github.com>
2020-11-15 11:15:53 +0100

    Username bij de Modify pagina
    Overige categoriëen zijn weggehaald

--- project-c/Areas/Identity/Pages/Account/Manage/Modify.cshtml.cs
+++ project-c/Areas/Identity/Pages/Account/Manage/Modify.cshtml.cs
@@ -36,6 +36,7 @@ namespace project_c.Areas.Identity.Pages.Account.Manage
             _logger = logger;
             _emailSender = emailSender;
         }
+        [Display(Name = "Gebruikersnaam")]
         public string Username { get; set; }
 
         public string Email { get; set; }
@@ -48,22 +49,45 @@ namespace project_c.Areas.Identity.Pages.Account.Manage
         [BindProperty]
         public EmailInputModel EmailInput { get; set; }
 
+        //User code.
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+                return Page();
+            }
+
+            await _signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Your profile has been updated";
+            return RedirectToPage();
+        }
+
+
+        //Email code.
         public class EmailInputModel
         {
-            [Required]
             [EmailAddress]
-            [Display(Name = "New email")]
+            [Display(Name = "Nieuwe email")]
             public string NewEmail { get; set; }
         }
 
         private async Task LoadAsync(User user)
         {
+            var userName = await _userManager.GetUserNameAsync(user);
             var email = await _userManager.GetEmailAsync(user);
+            Username = userName;
             Email = email;
 
             EmailInput = new EmailInputModel
             {
-                NewEmail = email,
             };
 
             IsEmailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
