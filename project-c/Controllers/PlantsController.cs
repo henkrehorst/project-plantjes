71c09c7bb493bf03f1e2534e6f5b94da5d6f33ba
Hamza Mehmood <hmehmood@hotmail.com>
2020-11-15 17:32:43 +0100

    Planten edit & details pagina toegevoegd

--- project-c/Controllers/PlantsController.cs
+++ project-c/Controllers/PlantsController.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using project_c.Models.Plants;
 
 namespace project_c.Controllers
 {
-
     public class PlantsController : Controller
     {
         private readonly DataContext _context;
@@ -16,31 +16,96 @@ namespace project_c.Controllers
         {
             _context = context;
         }
-        
-        // GET
-        public IActionResult Index()
+
+        // GET: PlantsController
+        public ActionResult Index()
         {
             var plants = from p in _context.Plants select p;
             return View(plants);
         }
 
-        [HttpGet]
-        public IActionResult Create()
+        // GET: PlantsController/Details/5
+        public ActionResult Details(int id)
+        {
+            var plant = from p in _context.Plants where p.PlantId == id select p;
+            return View(plant);
+        }
+
+        // GET: PlantsController/Create
+        public ActionResult Create()
         {
             return View();
         }
 
+        // POST: PlantsController/Create
         [HttpPost]
-        public IActionResult Create(string name, int length, string description)
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection form)
         {
-            Plant plant = new Plant();
-            plant.Name = name;
-            plant.Length = length;
-            plant.Description = description;
-
-            _context.Add(plant);
-            _context.SaveChanges();
-            return Content("Plant toegevoegd");
+            try
+            {
+                Plant plant = new Plant();
+                plant.Name = form["name"];
+                plant.Length = Convert.ToInt32(form["length"]);
+                plant.Description = form["description"];
+                _context.Add(plant);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
+        
+        // // GET: PlantsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var plant = from p in _context.Plants where p.PlantId == id select p;
+            return View(plant);
+        }
+        
+        // POST: PlantsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection form)
+        {
+            try
+            {
+                var plant = _context.Plants.Find(id);
+                
+                plant.Name = form["name"];
+                plant.Length = Convert.ToInt32(form["length"]);
+                plant.Description = form["description"];
+                _context.Update(plant);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        // // GET: PlantsController/Delete/5
+        // public ActionResult Delete(int id)
+        // {
+        //     return View();
+        // }
+        //
+        // // POST: PlantsController/Delete/5
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        // public ActionResult Delete(int id, IFormCollection collection)
+        // {
+        //     try
+        //     {
+        //         return RedirectToAction(nameof(Index));
+        //     }
+        //     catch
+        //     {
+        //         return View();
+        //     }
+        // }
     }
 }
