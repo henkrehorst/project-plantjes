5839f862d2e8262342191fb4f71a07df3583ede1
Hamza Mehmood <hmehmood@hotmail.com>
2020-11-16 21:45:20 +0100

    Eerste letter hoofdletter maken
    Eerste letter van naam en beschrijving wordt opgeslagen als hoofdletter

--- project-c/Controllers/PlantsController.cs
+++ project-c/Controllers/PlantsController.cs
@@ -28,7 +28,6 @@ namespace project_c.Controllers
                 var plant = from p in _context.Plants where p.Name.Contains(naam) select p;
                 return View(plant);
             }
-
             return View(plants);
         }
 
@@ -50,18 +49,21 @@ namespace project_c.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(IFormCollection form)
         {
+            var name = form["name"].ToString();
+            var description = form["description"].ToString();
+            description = char.ToUpper(description[0]) + description.Substring(1);
+            name = char.ToUpper(name[0]) + name.Substring(1);
             try
             {
                 Plant plant = new Plant();
                 if (ModelState.IsValid)
                 {
-                    plant.Name = form["name"];
+                    plant.Name = name;
                     plant.Length = Convert.ToInt32(form["length"]);
-                    plant.Description = form["description"];
+                    plant.Description = description;
                     _context.Add(plant);
                     _context.SaveChanges();
                 }
-
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -82,12 +84,16 @@ namespace project_c.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, IFormCollection form)
         {
+            var name = form["name"].ToString();
+            var description = form["description"].ToString();
+            description = char.ToUpper(description[0]) + description.Substring(1);
+            name = char.ToUpper(name[0]) + name.Substring(1);
             try
             {
                 var plant = _context.Plants.Find(id);
-                plant.Name = form["name"];
+                plant.Name = name;
                 plant.Length = Convert.ToInt32(form["length"]);
-                plant.Description = form["description"];
+                plant.Description = description;
                 _context.Update(plant);
                 _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
