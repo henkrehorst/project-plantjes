cd0b7e0139a9b1c4c4af5e05620f9d6e12787213
Hamza Mehmood <hmehmood@hotmail.com>
2020-11-16 14:58:53 +0100

    Change description field to textarea

--- project-c/Controllers/PlantsController.cs
+++ project-c/Controllers/PlantsController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using project_c.Models.Plants;
 
 namespace project_c.Controllers
