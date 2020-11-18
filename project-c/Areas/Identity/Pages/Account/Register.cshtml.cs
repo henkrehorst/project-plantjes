922ebbbf993118be6b50acec2f420f5cc400893c
mauricebunk <mauricebunk@users.noreply.github.com>
2020-11-18 13:00:22 +0100

    nieuwe navbar + knoppies

--- project-c/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ project-c/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -106,7 +106,7 @@ namespace project_c.Areas.Identity.Pages.Account
                         values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    await _emailSender.SendEmailAsync("mauricebunk@hotmail.com", "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
