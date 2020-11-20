9aadff7bf72faf9b3c08eeb500b0f2b6b61efba5
EHaze1999 <58138330+EHaze1999@users.noreply.github.com>
2020-11-20 10:56:55 +0100

    Chat begin
    Classes voor het chat systeem

---
+++ project-c/Models/Chat/Message.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_c.Models.Chat
+{
+    public class Message
+    {
+    }
+}
