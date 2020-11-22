db03c103c6b351920297bd5dd629ff50f79828af
EHaze1999 <58138330+EHaze1999@users.noreply.github.com>
2020-11-22 20:36:07 +0100

    chat css begin

--- project-c/Controllers/ChatController.cs
+++ project-c/Controllers/ChatController.cs
@@ -7,15 +7,19 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Data.SqlClient;
 using project_c.Models.Chat;
+using Microsoft.AspNetCore.Identity;
+using project_c.Models.Users;
+using System.Security.Claims;
 
 namespace project_c.Controllers
 {
     public class ChatController : Controller
     {
+        private readonly UserManager<User> _userManager;
         private readonly DataContext _context;
-
-        public ChatController(DataContext context)
+        public ChatController(UserManager<User> usr, DataContext context)
         {
+            _userManager = usr;
             _context = context;
         }
         public IActionResult Index()
@@ -23,9 +27,24 @@ namespace project_c.Controllers
             return View();
         }
 
-        public async Task<ActionResult> CreateChat(IFormCollection form, int receiver)
+        //GET: ChatController/Create
+        [Authorize]
+        public ActionResult Create()
         {
+            return View();
+        }
+
+        //Post: ChatController/Create
+        [Authorize]
+        public async Task<ActionResult> Create(IFormCollection form, int receiverID)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
             var msg = form["Message"].ToString();
+            var usr = form["User"];
             DateTime created = DateTime.Today;
             try
             {
@@ -34,7 +53,9 @@ namespace project_c.Controllers
                 {
                     chat.Created = created;
                     chat.ChatData = new ChatData();
-                    //chat.ChatData.Users.Add();
+                    chat.ChatData.Users.Add(user);
+                    _context.Add(chat);
+                    _context.SaveChanges();
                 }
                 return RedirectToAction(nameof(Index));
             }
--- project-c/Program.cs
+++ project-c/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using project_c.Models.Users;
 
 namespace project_c
 {
