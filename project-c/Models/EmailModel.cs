40381ce31eda1cbefd7f5cc3158d3897f7d16b4d
mauricebunk <mauricebunk@users.noreply.github.com>
2020-11-24 19:41:55 +0100

    Revert "test"
    This reverts commit 50b7fb8166851886a3614612cb1154ea21cbfd07.

--- project-c/Models/EmailModel.cs
+++ project-c/Models/EmailModel.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -23,4 +23,3 @@ namespace project_c.Models
         }
     }
 }
-*/
