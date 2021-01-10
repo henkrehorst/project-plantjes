7008b0ba58462e49d780546148e59632024fc705
nmslafk <55189572+nmslafk@users.noreply.github.com>
2021-01-10 20:37:22 +0100

    Update Register.cshtml.cs
    typfoutje

--- project-c/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ project-c/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -156,13 +156,13 @@ namespace project_c.Areas.Identity.Pages.Account
                         values: new {area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl},
                         protocol: Request.Scheme);
 
-                    using (MailMessage message = new MailMessage("ss@gmail.com", Input.Email))
+                    using (MailMessage message = new MailMessage("plantjesbuurt@gmail.com", Input.Email))
                     {
                         message.Subject = "Account verificatie";
                         message.Body = "\nHey " + Input.Voornaam + " " + Input.Achternaam +
                                        ",\n\n Je kunt je account bijna gebruiken! Klik alleen nog even op de onderstaande link" +
                                        "om je email te bevesitgen!\n\n" + callbackUrl +
-                                       "Groetjes,\n\n\nHet hele Plantjes Team!";
+                                       "Groetjes,\n\n\nPlantjesbuurt";
                         message.IsBodyHtml = false;
 
                         using (SmtpClient smtp = new SmtpClient())
